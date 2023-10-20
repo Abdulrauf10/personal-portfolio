b2f7ed1afa226c301c6bbfb825651fcb4f50f805
Abdulrauf10 <abd.rauf.lamada@gmail.com>
2023-10-20 17:21:56 +0700

    changed layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,81 +2,58 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import { motion } from 'framer-motion'
+import About from '@/components/About'
+import Portfolio from '@/components/Portfolio'
 
 export default function Home() {
   return (
-    <motion.div
-      className="box"
-      initial={{ opacity: 0, scale: 0.5 }}
-      animate={{ opacity: 1, scale: 1 }}
-      transition={{
-        duration: 0.8,
-        delay: 0.5,
-        ease: [0, 0.71, 0.2, 1.01]
-      }}
-    >
+    <>
       <div className="bg-primary h-screen w-screen flex items-center justify-center sx:max-sm:h-fit sx:max-sm:pt-20">
-        <div className="bg-secondary w-3/4 h-3/4 flex items-center justify-center rounded sx:max-sm:flex-col sx:max-sm:justify-start">
-          <Image
-            src="/images/avatar.svg"
-            alt="avatar"
-            width={200}
-            height={200}
-            className="flex sx:max-sm:w-40"
-          />
-          <div className="sx:max-sm:text-center">
-            <div className="-my-40 sx:max-sm:my-0">
-              <h3 className="text-6xl text-text-color sx:max-sm:text-2xl">
-                Hi, there!
-              </h3>
-              <p className="text-text-color">
-                Welcome to my website. I am Rauf, a full-stack web developer
-              </p>
-              <button
-                className="bg-text-color p-2 rounded-md text-primary mt-5"
-                type="button"
-              >
-                <Link href={'/about'}>Learn More</Link>
-              </button>
-            </div>
-          </div>
+        <motion.div
+          className="box bg-primary h-screen w-screen flex items-center justify-center sx:max-sm:h-fit sx:max-sm:pt-20"
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{
+            duration: 0.3,
+            ease: [0, 0.71, 0.2, 1.01],
+            scale: {
+              type: 'spring',
+              damping: 5,
+              stiffness: 100,
+              restDelta: 0.001
+            }
+          }}
+        >
+          <div className="bg-secondary w-3/4 h-3/4 flex items-center justify-center rounded sx:max-sm:flex-col sx:max-sm:justify-start">
+            <Image
+              src="/images/avatar.svg"
+              alt="avatar"
+              width={200}
+              height={200}
+              className="flex sx:max-sm:w-40"
+            />
 
-          <h2 className="text-text-color text-lg absolute right-0 bottom-0 mb-72 mr-64 sx:max-sm:flex sx:max-sm:mr-0 sx:max-sm:mt-10 sx:max-sm:relative sx:max-sm:mb-3">
-            Current project:
-          </h2>
-          <div className="absolute bottom-0 right-0 mb-12 mr-12 flex sx:max-sm:relative sx:max-sm:items-center sx:max-sm:flex-col sx:max-sm:mr-0 ">
-            <div className="card card-compact w-40 h-56 glass shadow-xl mr-5 sx:max-sm:mr-0 sx:max-sm:mb-5">
-              <figure>
-                <img src="/images/flexibble.png" alt="flexibble" />
-              </figure>
-              <div className="card-body">
-                <h2 className="card-title">Flexibble</h2>
-                <p>Web app for portfolio showcase</p>
-                <div className="card-actions justify-end">
-                  <button className="p-2 bg-primary text-text-color rounded-md">
-                    <Link href={'/portfolio'}>Learn more</Link>
-                  </button>
-                </div>
-              </div>
-            </div>
-
-            <div className="card card-compact w-40 h-56 glass shadow-xl ">
-              <figure>
-                <img src="/images/react.jpg" alt="flexibble" />
-              </figure>
-              <div className="card-body">
-                <h2 className="card-title">Job-portal</h2>
-                <p>Mobile app for finding job</p>
-                <div className="card-actions justify-end">
-                  <button className="p-2 bg-primary text-text-color rounded-md">
-                    <Link href={'/portfolio'}>Learn more</Link>
-                  </button>
-                </div>
+            <div className="sx:max-sm:text-center flex flex-wrap w-6/12">
+              <div className="-my-40 sx:max-sm:my-0  ">
+                <h3 className="text-3xl text-text-color sx:max-sm:text-2xl">
+                  Hi there, Rauf's here...
+                </h3>
+                <br />
+                <br />
+                <p className="text-text-color">
+                  Welcome to my website. Discover how I leverage web development
+                  skills to address your business challenges effectively.
+                  Elevate your brand, improve user experiences, and drive growth
+                  with my expertise
+                </p>
               </div>
             </div>
           </div>
-        </div>
+        </motion.div>
       </div>
-    </motion.div>
+
+      <About />
+      <Portfolio />
+    </>
   )
 }
---
+++ src/components/Portfolio.tsx
@@ -0,0 +1,72 @@
+import Link from 'next/link'
+
+const Portfolio = () => {
+  return (
+    <div className="bg-primary p-24 h-fit w-screen items-center justify-center  sx:max-sm:h-fit sx:max-sm:pt-20">
+      <h3 className="text-text-color text-center mb-10 text-3xl sx:max-sm:text-2xl">
+        Current Projects
+      </h3>
+      <div className="flex justify-center sx:max-sm:flex-col">
+        <div className="card lg:card-side bg-base-100 shadow-xl mr-8 sx:max-sm:mb-8 ">
+          <figure>
+            <img src="/images/flexibbleApp.png" alt="Album" />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title sx:max-sm:text-center">
+              Web App: Flexibble
+            </h2>
+            <p className="sx:max-sm:text-center">
+              This is a website where users can upload their personal
+              portfolios, including image, a links to live website, and GitHub
+              URL
+              <br />
+              Technologies: Next.Js, GraphQL, Grafbase, Tailwind
+            </p>
+            <div className="card-actions justify-end flex sx:max-sm:justify-center">
+              <button className="btn btn-primary">
+                <a href="https://port-flexibble.vercel.app/" target="_blank">
+                  Go to website
+                </a>
+              </button>
+              <button className="btn btn-primary">
+                <a
+                  href="https://github.com/Abdulrauf10/flexibble"
+                  target="_blank"
+                >
+                  Source code
+                </a>
+              </button>
+            </div>
+          </div>
+        </div>
+
+        <div className="card lg:card-side bg-base-100 shadow-xl">
+          <figure>
+            <img src="/images/portal.png" alt="Album" />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title sx:max-sm:text-center">
+              Mobile App React Native: Job Portal
+            </h2>
+            <p className="sx:max-sm:text-center">
+              Application to find jobs. To run the code in your own mobile, you
+              have to install expo go for android or camera app for ios
+            </p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">
+                <a
+                  href="https://github.com/Abdulrauf10/job-portal-reactnative"
+                  target="_blank"
+                >
+                  Source code
+                </a>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Portfolio
